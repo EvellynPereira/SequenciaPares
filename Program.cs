b60ed0693c9c24a86b833d3e4b781773945bf477
evellyn <evellynl464@gmail.com>
2022-11-21 22:21:21 -0300

    atividade

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vamos de novidades ???");
+Console.Write("Números pares entre 0 e ? ");
+
+int numeroInformado = Convert.ToInt32(Console.ReadLine());
+
+
+if (numeroInformado >= 1)
+
+
+{int numeroPar = 0;
+
+
+while (numeroPar <= numeroInformado)
+
+{ Console.WriteLine($"{numeroPar} ");
+
+numeroPar += 2;}}
+
+
+else
+
+
+{Console.Write($"O número {numeroInformado} não é inteiro positivo.");}
